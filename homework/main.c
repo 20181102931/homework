6ab232079c666ff4f0bf017b125731aac0a4f17c
20181102931 <18647197975@163.com>
2019-10-22 16:00:08 +0800

    Initial Commit

---
+++ homework/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  homework
+//
+//  Created by s20181102931 on 2019/10/22.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
